d291c1df55db0ab522a5cc5c212dbc8e6c57b154
zhang <zhang@zhangdeMacBook-Pro.local>
2017-03-08 10:56:24 +0800

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="scss">
+$backgroundWhite: #2c3e50;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $backgroundWhite;
+  margin-top: 60px;
+}
+</style>
